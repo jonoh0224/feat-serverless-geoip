483bdd712bd29886d8c80bcbd3d98a4135f9eb59
jonghyuk oh <jonoh0224@gmail.com>
2024-02-29 16:59:20 +0900

    aws-cdk 버전업. 배포완료

--- bin/quick-and-dirty-serverless-geoip-api.ts
+++ bin/quick-and-dirty-serverless-geoip-api.ts
@@ -1,42 +1,91 @@
 #!/usr/bin/env node
-import * as cdk from '@aws-cdk/core';
-import { QuickAndDirtyServerlessGeoipApiStack } from '../lib/quick-and-dirty-serverless-geoip-api-stack';
+import * as cdk from "aws-cdk-lib";
+import { QuickAndDirtyServerlessGeoipApiStack } from "../lib/quick-and-dirty-serverless-geoip-api-stack";
 
-import * as os from 'os';
-import * as path from 'path';
-import * as https from 'https';
-import * as fs from 'fs';
-import * as decompress from 'decompress';
+import * as os from "os";
+import * as path from "path";
+import * as https from "https";
+import * as fs from "fs";
+import * as decompress from "decompress";
+import { URL } from "url";
 
 const app = new cdk.App();
 
 // Generate a YYYYMMDD date stamp to use in the filename of the temp downloaded MaxMind GeoLite2 City database
-const dateStamp = new Date().getFullYear() + // Year
-                    ("0" + (new Date().getMonth() + 1)).slice(-2) + // Month
-                    ("0" + new Date().getDate()).slice(-2); // Day
-const tempCityDatabaseDownloadFileName = `${fs.mkdtempSync(path.join(os.tmpdir(), 'qdsgeoipapi-'))}/GeoLite2-City_${dateStamp}.tar.gz`;
+const dateStamp =
+  new Date().getFullYear() + // Year
+  ("0" + (new Date().getMonth() + 1)).slice(-2) + // Month
+  ("0" + new Date().getDate()).slice(-2); // Day
+const tempCityDatabaseDownloadFileName = `${fs.mkdtempSync(
+  path.join(os.tmpdir(), "qdsgeoipapi-")
+)}/GeoLite2-City_${dateStamp}.tar.gz`;
 
-// Download the MaxMind GeoLite2 City database
-const tempCityDatabaseDownloadFile = fs.createWriteStream(tempCityDatabaseDownloadFileName);
-console.log(`Retrieving compressed MaxMind GeoLite2 City database to ${tempCityDatabaseDownloadFileName} ...`);
-https.get(`https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=${app.node.tryGetContext('maxMindLicenseKey')}&suffix=tar.gz`, function(response) {
-    response.pipe(tempCityDatabaseDownloadFile);
-});
+// Function to handle download and follow redirects if necessary
+function downloadFile(
+  url: string | https.RequestOptions | URL | undefined,
+  outputPath: fs.PathLike,
+  callback: { (): void; (): void }
+) {
+  https
+    .get(url as string, (response) => {
+      console.log(`Status Code for download: ${response.statusCode}`);
+      if (response.statusCode === 302 || response.statusCode === 301) {
+        // If redirected, download from the new location
+        const newUrl = response.headers.location;
+        console.log(`Redirected to ${newUrl}`);
+        downloadFile(newUrl, outputPath, callback);
+      } else if (response.statusCode === 200) {
+        // If successfully responded, write the file
+        const fileStream = fs.createWriteStream(outputPath);
+        response.pipe(fileStream);
+        fileStream.on("finish", () => {
+          fileStream.close();
+          console.log("Download Completed");
+          callback(); // Call the callback function once download is complete
+        });
+      } else {
+        console.error(
+          `Download failed with status code: ${response.statusCode}`
+        );
+      }
+    })
+    .on("error", (err) => {
+      console.error("Download Error:", err);
+    });
+}
 
-tempCityDatabaseDownloadFile.on('finish', () => {
-
-  // Extract the downloaded MaxMind GeoLite2 City database
-  console.log(`Extracting MaxMind GeoLite2 City database to ../lambda/GeoLite2-City.mmdb ...`);
-  decompress(tempCityDatabaseDownloadFileName, path.join(__dirname, '../lambda'), {
-      filter: file => path.extname(file.path) == '.mmdb',
-      strip: 1
-  }).then(files => {
-
-      // Now that we've retrieved, extracted, and placed the MaxMind GeoLite2 City database into our Lambda function directory
-      // We can now synth/deploy our apps
-      new QuickAndDirtyServerlessGeoipApiStack(app, 'QuickAndDirtyServerlessGeoipApiStack');
-
-  });
-
-})
-
+// Start the download
+console.log(
+  `Retrieving compressed MaxMind GeoLite2 City database to ${tempCityDatabaseDownloadFileName} ...`
+);
+downloadFile(
+  `https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=${app.node.tryGetContext(
+    "maxMindLicenseKey"
+  )}&suffix=tar.gz`,
+  tempCityDatabaseDownloadFileName,
+  () => {
+    // Extract the downloaded MaxMind GeoLite2 City database after download
+    console.log(
+      `Extracting MaxMind GeoLite2 City database to ../lambda/GeoLite2-City.mmdb ...`
+    );
+    decompress(
+      tempCityDatabaseDownloadFileName,
+      path.join(__dirname, "../lambda"),
+      {
+        filter: (file) => path.extname(file.path) === ".mmdb",
+        strip: 1,
+      }
+    )
+      .then((files) => {
+        console.log("Extracted files:", files);
+        // Initialize the AWS CDK stack after the database is extracted
+        new QuickAndDirtyServerlessGeoipApiStack(
+          app,
+          "QuickAndDirtyServerlessGeoipApiStack"
+        );
+      })
+      .catch((error) => {
+        console.error("Decompression error:", error);
+      });
+  }
+);
--- lib/quick-and-dirty-serverless-geoip-api-stack.ts
+++ lib/quick-and-dirty-serverless-geoip-api-stack.ts
@@ -1,50 +1,62 @@
-import * as cdk from '@aws-cdk/core';
-import * as lambda from '@aws-cdk/aws-lambda';
-import * as lambda_nodejs from '@aws-cdk/aws-lambda-nodejs'
-import * as apigatewayv2 from '@aws-cdk/aws-apigatewayv2'
-import * as apigatewayv2_integrations from '@aws-cdk/aws-apigatewayv2-integrations'
-import * as path from 'path';
+import {
+  Stack,
+  App,
+  StackProps,
+  Duration,
+  CfnOutput,
+  aws_apigatewayv2_integrations,
+  aws_lambda,
+  aws_lambda_nodejs,
+  aws_apigatewayv2,
+} from "aws-cdk-lib";
 
-export class QuickAndDirtyServerlessGeoipApiStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+import * as path from "path";
+
+export class QuickAndDirtyServerlessGeoipApiStack extends Stack {
+  constructor(scope: App, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const geoIPLookupFunction = new lambda_nodejs.NodejsFunction(this, 'geoIPLookup', {
-      runtime: lambda.Runtime.NODEJS_14_X,
-      entry: path.join(__dirname, '../lambda/index.js'),
-      handler: 'handler',
-      timeout: cdk.Duration.seconds(25),
-      memorySize: 512,
-      bundling: {
-        commandHooks: {
-          // Copy a file so that it will be included in the bundled asset
-          afterBundling(inputDir: string, outputDir: string): string[] {
-            return [`cp ${inputDir}/lambda/GeoLite2-City.mmdb ${outputDir}`];
+    const geoIPLookupFunction = new aws_lambda_nodejs.NodejsFunction(
+      this,
+      "geoIPLookup",
+      {
+        runtime: aws_lambda.Runtime.NODEJS_20_X,
+        entry: path.join(__dirname, "../lambda/index.js"),
+        handler: "handler",
+        timeout: Duration.seconds(25),
+        memorySize: 512,
+        bundling: {
+          commandHooks: {
+            // Copy a file so that it will be included in the bundled asset
+            afterBundling(inputDir: string, outputDir: string): string[] {
+              return [`cp ${inputDir}/lambda/GeoLite2-City.mmdb ${outputDir}`];
+            },
+            beforeInstall(inputDir: string, outputDir: string): string[] {
+              return [];
+            },
+            beforeBundling(inputDir: string, outputDir: string): string[] {
+              return [];
+            },
           },
-          beforeInstall(inputDir: string, outputDir: string): string[] {
-            return [];
-          },
-          beforeBundling(inputDir: string, outputDir: string): string[] {
-            return [];
-          }
-        }
+        },
       }
-    });
+    );
 
-    const httpApi = new apigatewayv2.HttpApi(this, 'HttpApi');
+    const httpApi = new aws_apigatewayv2.HttpApi(this, "HttpApi");
 
-    const geoIPLookupIntegration = new apigatewayv2_integrations.LambdaProxyIntegration({
-      handler: geoIPLookupFunction,
-    });
+    const geoIPLookupIntegration =
+      new aws_apigatewayv2_integrations.HttpLambdaIntegration(
+        "geoIPLookupIntegration",
+        geoIPLookupFunction
+      );
     httpApi.addRoutes({
-      path: '/',
-      methods: [ apigatewayv2.HttpMethod.GET ],
+      path: "/",
+      methods: [aws_apigatewayv2.HttpMethod.GET],
       integration: geoIPLookupIntegration,
     });
 
-    new cdk.CfnOutput(this, 'sampleApiEndpoint', {
-      value: httpApi.apiEndpoint
-    })
-
+    new CfnOutput(this, "sampleApiEndpoint", {
+      value: httpApi.apiEndpoint,
+    });
   }
 }
